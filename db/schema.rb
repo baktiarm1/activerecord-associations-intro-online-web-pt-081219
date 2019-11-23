71d210e871f06b1bd25212bbe44804b08045bc9c
Baktiar Miah <baktiar.m@gmail.com>
2019-11-23 17:08:53 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,6 @@ ActiveRecord::Schema.define(version: 4) do
     t.string "name"
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-  end
-
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
